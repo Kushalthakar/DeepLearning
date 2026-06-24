2dc696378663d9f33a810783e3802ac0f4af34b5
Kushalkumar Thakar <kushalthakar555@gmail.com>
2026-06-23 23:43:43 -0500

    Report + Confusion Matrix + Predictions + Save Model + Fix Errors

--- Assignment/CNNOnCIFAR10DataSet.py
+++ Assignment/CNNOnCIFAR10DataSet.py
@@ -17,7 +17,7 @@ print("TensorFlow Version:", tf.__version__)
 print("Keras Version:", keras.__version__)
 print("PyTorch Version:", torch.__version__)
 
-# Reproducable and output folder
+# Reproducible and output folder
 SEED = 42
 random.seed(SEED)
 np.random.seed(SEED)
@@ -61,7 +61,7 @@ print("Test Pixel min/Max:", yTest.min(), yTest.max())
 print("Training count:")
 
 for i, name in enumerate(CLASS_NAMES):
-    print(f"{i}: {name: 10s} -> {np.sum(yTrainFull == i)}")
+    print(f"{i}: {name:10s} -> {np.sum(yTrainFull == i)}")
 
 # Image grid
 def saveGrid(images, labels, classNames, fileName):
@@ -71,7 +71,7 @@ def saveGrid(images, labels, classNames, fileName):
         plt.title(classNames[labels[i]], fontsize = 10)
         plt.axis("off")
     plt.tight_layout()
-    plt.savefig(fileName, dip = 200)
+    plt.savefig(fileName, dpi = 200)
     plt.close()
 
 saveGrid(xTrainFull,
@@ -95,10 +95,10 @@ print("Test label min/max:", yTest.min(), yTest.max())
 
 # Validation Split
 xTrain, xVal, yTrain, yVal = train_test_split(xTrainFull,
-                                          yTrainFull,
-                                          test_size = 0.10,
-                                          random_state = SEED,
-                                          stratify = yTrainFull)
+                                              yTrainFull,
+                                              test_size = 0.10,
+                                              random_state = SEED,
+                                              stratify = yTrainFull)
 
 print("x train:", xTrain.shape, "y train:", yTrain.shape)
 print("x val:", xVal.shape, "y val:", yVal.shape)
@@ -119,11 +119,11 @@ def cnnModel(inputShape = (32, 32, 3), numClasses = 10):
     x = dataAugmentation(inputs)
 
     # Block 1 (32 filters)
-    x = layers.Conv2D(32, (3,3), padding = "same", kernel_regularizer = 12)(x)
+    x = layers.Conv2D(32, (3,3), padding = "same", kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relue")(x)
+    x = layers.Activation("relu")(x)
 
-    x = layers.Conv2D(32, (3,3), padding = "same", kernel_regularizer = 12)(x)
+    x = layers.Conv2D(32, (3,3), padding = "same", kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
 
@@ -131,11 +131,11 @@ def cnnModel(inputShape = (32, 32, 3), numClasses = 10):
     x = layers.Dropout(0.20)(x)
 
     # Block 2 (64 Filters)
-    x = layers.Conv2D(64, (3,3), padding = "same", kernel_regularizer = 12)(x)
+    x = layers.Conv2D(64, (3,3), padding = "same", kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation("relue")(x)
+    x = layers.Activation("relu")(x)
 
-    x = layers.Conv2D(64, (3,3), padding = "same", kernel_regularizer = 12)(x)
+    x = layers.Conv2D(64, (3,3), padding = "same", kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
     
@@ -143,20 +143,20 @@ def cnnModel(inputShape = (32, 32, 3), numClasses = 10):
     x = layers.Dropout(0.30)(x)
 
     # Block 3 (128 Filters)
-    x = layers.Conv2D(128, (3,3), kernel_regularizer=12)(x)
+    x = layers.Conv2D(128, (3,3), kernel_regularizer=l2)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
 
-    x = layers.Conv2D(128, (3,3), kernel_regularizer = 12)(x)
+    x = layers.Conv2D(128, (3,3), kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
-    x = layers.Activation()(x)
+    x = layers.Activation("relu")(x)
 
     x = layers.MaxPooling2D((2,2))(x)
     x = layers.Dropout(0.40)(x)
 
     # Classifier
     x = layers.Flatten()(x)
-    x = layers.Dense(256, kernel_regularizer = 12)(x)
+    x = layers.Dense(256, kernel_regularizer = l2)(x)
     x = layers.BatchNormalization()(x)
     x = layers.Activation("relu")(x)
     x = layers.Dropout(0.50)(x)
@@ -171,32 +171,32 @@ model = cnnModel()
 model.summary()
 
 # Configure
-model.compile(optimizer = tf.keras.optimizers.Adam(learningRate = 0.001),
-              loss = "sparseCategoricalCrossentropy",
+model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001),
+              loss = "sparse_categorical_crossentropy",
               metrics = ["accuracy"])
 
 # Train Model
-BATCH_SIZE = 64
-EPOCHS = 15
+BATCH_SIZE = 75
+EPOCHS = 2
 
-goodModelPath = os.path.join(DIR, "goodCifar10Cnn.keras")
+good_model_path = os.path.join(DIR, "goodCifar10Cnn.keras")
 
-callback = [
+callbacks = [
     tf.keras.callbacks.ModelCheckpoint(
-        filepath = goodModelPath,
-        monitor = "val accuracy",
+        filepath = good_model_path,
+        monitor = "val_accuracy",
         mode = "max",
-        saveOnly = True,
+        save_best_only = True,
         verbose = 1),
     tf.keras.callbacks.EarlyStopping(
-        monitor = "val accuracy",
+        monitor = "val_accuracy",
         mode = "max",
         patience = 7,
         restore_best_weights = True,
         verbose = 1),
     tf.keras.callbacks.ReduceLROnPlateau(
-        monitor = "val loss",
-        factor = "0.5",
+        monitor = "val_loss",
+        factor = 0.5,
         patience = 3,
         min_lr = 1e-6,
         verbose = 1)
@@ -207,7 +207,7 @@ history = model.fit(xTrain,
                     validation_data = (xVal, yVal),
                     epochs = EPOCHS,
                     batch_size = BATCH_SIZE,
-                    callbacks = callback,
+                    callbacks = callbacks,
                     verbose = 1)
 
 # Training Curve
@@ -218,7 +218,7 @@ def saveTrainingCurves(history, fileName):
     plt.figure(figsize = (10, 4))
     plt. subplot(1, 2, 1)
     plt.plot(epochsRange, hist["accuracy"], label = "Training Accuracy")
-    plt.plot(epochsRange, hist["val accuracy"], label = "Validation Accuracy")
+    plt.plot(epochsRange, hist["val_accuracy"], label = "Validation Accuracy")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.title("Training vs Validation Accuracy")
@@ -241,7 +241,68 @@ saveTrainingCurves(history, trainingCurvesPath)
 print(f"\nSaves curves to {trainingCurvesPath}")
 
 # Final Evaluation
-print("\n Test Evaluation")
-testLoss, testAccuracy = model.evaluate(xTest,yTest, batchSize = BATCH_SIZE, verbose = 0)
+print("\nEvaluation")
+testLoss, testAccuracy = model.evaluate(xTest, yTest, batch_size = BATCH_SIZE, verbose = 0)
 print(f"Test Loss: {testLoss: .4f}")
 print(f"Test Accuracy: {testAccuracy: .4f}")
+
+# Prediction, Classification Report and Confusion Matrix
+yProb = model.predict(xTest, batch_size = BATCH_SIZE, verbose = 0)
+yPred = np.argmax(yProb, axis = 1)
+
+print("\nClassification Report")
+print(classification_report(yTest, yPred, target_names = CLASS_NAMES))
+
+cm = confusion_matrix(yTest, yPred)
+plt.figure(figsize = (9,9))
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = CLASS_NAMES)
+disp.plot(values_format = "d", xticks_rotation = 45)
+plt.title("CIFAR-10 Confusion Matrix")
+plt.tight_layout()
+
+confusionMatrixPath = os.path.join(DIR, "confusionMatrix.png")
+plt.savefig(confusionMatrixPath, dpi = 200)
+plt.close()
+print(f"saved confusion matrix to {confusionMatrixPath}")
+
+# Save correct predictions
+def savePredictions(images, trueLabel, predLabel, classNames, filename):
+    correctId = np.where(trueLabel == predLabel)[0]
+    incorrectId = np.where(trueLabel != predLabel)[0]
+
+    correctSample = correctId[:8]
+    incorrectSample = incorrectId[:8]
+
+    plt.figure(figsize = (12, 6))
+
+    for i, id in enumerate(correctSample):
+        plt.subplot(2, 8, i + 1)
+        plt.imshow(images[id])
+        plt.title(f"T: {classNames[trueLabel[id]]}\nP: {classNames[predLabel[id]]}",
+                  fontsize = 8)
+        plt.axis("off")
+
+    for i, id in enumerate(incorrectSample):
+        plt.subplot(2, 8, i + 9)
+        plt.imshow(images[id])
+        plt.title(f"T: {classNames[trueLabel[id]]}\nP: {classNames[predLabel[id]]}",
+                  fontsize = 8)
+        plt.axis("off")
+    
+    plt.suptitle("Top row correct Predictions | Bottom row incorrect prediction")
+    plt.tight_layout()
+    plt.savefig(filename, dpi= 200)
+    plt.close()
+
+predictionPath = os.path.join(DIR, "prediction.png")
+savePredictions(xTest,
+                yTest,
+                yPred,
+                CLASS_NAMES,
+                predictionPath)
+print(f"Saved correct predection: {predictionPath}")
+
+# Save Model
+finalModel = os.path.join(DIR, "finalCifar10Cnn.keras")
+model.save(finalModel)
+print(f"Save final model to {finalModel}")
